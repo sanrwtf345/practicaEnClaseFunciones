2be74890cb7d5db2ca9dba03bfa823e4262164d4
Santi <sanromcast@gmail.com>
2025-05-08 11:52:02 -0300

    commit 1

---
+++ src/ejercicios/Ejercicio2.java
@@ -0,0 +1,11 @@
+package ejercicios;
+
+public class Ejercicio2 {
+  /*Ejercicio 2:   Generar un array con números aleatorios (entre 1 y 100) y permitir al usuario buscar un número en él.
+Función para generar el array con números aleatorios (generarArray()).
+Función para buscar un número y devolver su posición (buscarNumero()).
+Función para ordenar el array (ordenarArray()).
+Función para mostrar el array (mostrarArray()).*/
+
+}
+
---
+++ src/ejercicios/Main.java
@@ -0,0 +1,66 @@
+package ejercicios;
+
+import java.util.Scanner;
+import java.util.logging.Logger;
+
+
+public class Main {
+  /*Ejercicio 1: Crear un programa que permita ingresar las notas de  estudiantes y calcule:
+El promedio de notas.
+La nota más alta.
+La nota más baja.
+Cantidad de aprobados (nota ≥ 6).
+Usar un array para almacenar las notas.
+Funciones para cada cálculo (calcularPromedio(), encontrarMaxima(), etc.).
+Validar que las notas estén entre 0 y 10*/
+  public static final Logger log = Logger.getLogger(Main.class.getName());
+  public static void main(String[] args) {
+    //int cantidadEstudiantes= ingresarNro("la cantidad de estudiantes");
+    int cantidadNotas= ingresarNro("la cantidad de notas");
+
+    int[] notas= new int [cantidadNotas];
+
+    cargarNotasEnArray(notas);
+    mostrarNotas(notas);
+    log.info("La nota mas alta " +encontrarElMayor(notas));
+    log.info("La nota mas baja " +encontrarElMenor(notas));
+    log.info("El promedio es " +calcularPromedio(notas));
+
+  }
+ public static int ingresarNro(String mensaje) {
+    int nro;
+    log.info("Por favor ingrese la cantidad de "+mensaje);
+    Scanner sc = new Scanner(System.in);
+    nro=sc.nextInt();
+    return nro;
+ }
+ public static void cargarNotasEnArray(int[] notas) {
+    for (int i=0; i< notas.length; i++){
+      notas[i]= ingresarNro("la nota del estudiante "+(i+1));
+    }
+ }
+ public static void mostrarNotas(int[] notas) {
+    for (int i=0; i< notas.length; i++){
+      log.info("La nota "+ (i+1) + "es " +notas[i]);
+    }
+ }
+ public static int encontrarElMayor(int[] notas) {
+    int mayor=0;
+    for (int i=0; i< notas.length; i++){
+      mayor=(notas[i]>mayor)? notas[i]: mayor;
+    }return mayor;
+ }
+  public static int encontrarElMenor(int[] notas) {
+    int menor=10;
+    for (int i=0; i< notas.length; i++){
+      menor=(notas[i]<menor)? notas[i]: menor;
+    }return menor;
+  }
+  public static int calcularPromedio(int[] notas) {
+    int promedio = 0;
+    for (int i=0; i< notas.length; i++) {
+
+
+    }return promedio;
+  }
+}
